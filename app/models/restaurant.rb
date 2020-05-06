04fd24086b0b5825cf356e3b1a55822ea13be597
Benjamin Steidl <localartpool@gmail.com>
2020-05-06 14:32:46 +0200

    Add validation to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: ["chinese", "italian", "japanese", "french", "belgian"]
 end
