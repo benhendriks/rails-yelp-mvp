7d485f94457b1ca1fa2003962714b1388b185a30
Benjamin Steidl <localartpool@gmail.com>
2020-05-06 15:10:04 +0200

    Add the review and connect it with the perent table

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+  def new
+  end
+  def create
+  end
+  def destroy
+  end
+
+private
+
+  def set_restaurant
+  end
+  def rewie_params
+  end
+end
